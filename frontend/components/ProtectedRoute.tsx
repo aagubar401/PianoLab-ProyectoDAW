8856fb1c8e56bb0c3d3a43168f300f881741e62d
aagubar401 <aagubar401@fp.iesromerovargas.com>
2026-04-09 18:06:47 +0200

    arreglar ProtectedRoute

--- frontend/components/ProtectedRoute.tsx
+++ frontend/components/ProtectedRoute.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { ReactNode, useEffect } from "react";
-import { useAuth } from "../lib/auth";
+import { useAuth } from "../lib/useAuth";
 
 export default function ProtectedRoute({ children }: { children: ReactNode }) {
   const { user, loading } = useAuth();
